630c290c16238ac2ea96d3e3b438df7049c208a9
Mark Wilson <markwilson@reflexgaming.com>
2024-11-07 13:17:14 +0000

    Putting strings into PROGMEM.

--- src/MPU6050_tockn.cpp
+++ src/MPU6050_tockn.cpp
@@ -56,13 +56,13 @@ void MPU6050::calcGyroOffsets(bool console, uint16_t delayBefore, uint16_t delay
   delay(delayBefore);
 	if(console){
     Serial.println();
-    Serial.println("========================================");
-    Serial.println("Calculating gyro offsets");
-    Serial.print("DO NOT MOVE MPU6050");
+    Serial.println(F("========================================"));
+    Serial.println(F("Calculating gyro offsets"));
+    Serial.print(F("DO NOT MOVE MPU6050"));
   }
   for(int i = 0; i < 3000; i++){
     if(console && i % 1000 == 0){
-      Serial.print(".");
+      Serial.print(F("."));
     }
     wire->beginTransmission(MPU6050_ADDR);
     wire->write(0x43);
@@ -83,12 +83,12 @@ void MPU6050::calcGyroOffsets(bool console, uint16_t delayBefore, uint16_t delay
 
   if(console){
     Serial.println();
-    Serial.println("Done!");
-    Serial.print("X : ");Serial.println(gyroXoffset);
-    Serial.print("Y : ");Serial.println(gyroYoffset);
-    Serial.print("Z : ");Serial.println(gyroZoffset);
-    Serial.println("Program will start after 3 seconds");
-    Serial.print("========================================");
+    Serial.println(F("Done!"));
+    Serial.print(F("X : "));Serial.println(gyroXoffset);
+    Serial.print(F("Y : "));Serial.println(gyroYoffset);
+    Serial.print(F("Z : "));Serial.println(gyroZoffset);
+    Serial.println(F("Program will start after 3 seconds"));
+    Serial.print(F("========================================"));
 		delay(delayAfter);
 	}
 }
